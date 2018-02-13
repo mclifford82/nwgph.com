176d5c4f1d6b486c5ffb0f766877f0214ee4d924
Martin Clifford <mclifford82@gmail.com>
2018-02-12 23:46:30 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AddNewPost from './components/AddNewPost.js';
+
+class App extends Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
+
+export default App;
